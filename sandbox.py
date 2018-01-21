60dabbee6e7ae9b90c88be26a772284a45bb8fb6
Josh Goldberg <josh.goldberg1@outlook.com>
2018-01-20 18:25:49 -0600

    Lesson progress

--- sandbox.py
+++ sandbox.py
@@ -1,11 +1,19 @@
-counts = {1963: 2, 1964: 3, 1965: 2, 1966: 1, 1967: 2, 1968: 1, 1969: 2, 1970: 1}
+# counts = {1963: 2, 1964: 3, 1965: 2, 1966: 1, 1967: 2, 1968: 1, 1969: 2, 1970: 1}
+#
+#
+# def my_lambda(count_index):
+#     print('debug: count_index: {}'.format(count_index))
+#     print('debug: counts[count_index]: {}'.format(counts[count_index]))
+#     return counts[count_index]
+#
+# print(max(counts, key=my_lambda))
 
 
-def my_lambda(count_index):
-    print('debug: count_index: {}'.format(count_index))
-    print('debug: counts[count_index]: {}'.format(counts[count_index]))
-    return counts[count_index]
-
-print(max(counts, key=my_lambda))
+egg_count = 0
 
 
+def buy_eggs():
+    egg_count += 12  # purchase a dozen eggs
+
+
+buy_eggs()
