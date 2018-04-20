c3a82312ac2958995d2e78bd66e0a88b79958b5d
Josh Goldberg <josh.goldberg1@outlook.com>
2018-04-20 01:28:02 -0500

    Lesson progress

--- Lesson_3_data_structures_loops/break_string_v2.py
+++ Lesson_3_data_structures_loops/break_string_v2.py
@@ -1,3 +1,11 @@
+# Time to write your own loop with a break statement. Your task is to create a string, news_ticker that is exactly
+# 140 characters long. You should create the news ticker by adding headlines from the headlines list, inserting a
+# space in between each headline. If necessary, truncate the last headline in the middle so that news_ticker is
+# exactly 140 characters long.
+
+# Remember that break works in both for and while loops. Use whichever loop seems most appropriate. Consider adding
+# print statements to your code to help you resolve bugs.
+
 # HINT: modify the headlines list to verify your loop works with different inputs
 headlines = ["Local Bear Eaten by Man",
              "Legislature Announces New Laws",
